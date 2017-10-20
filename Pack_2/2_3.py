383cbe205d2c6b721423479c5e4c1ed36b0848d3
tweetklim <tweetklim@gmail.com^Cgit config --global user.email tweetklim@gmail.com>
2017-10-21 02:44:05 +0300

    1

--- Pack_2/2_3.py
+++ Pack_2/2_3.py
@@ -1,11 +1,16 @@
 import copy
 n = int(input())
 a = [1]
-print(a)
+t = " "
+print(t * (n+1), end="")
+print(a[0])
 for i in range(0,n):
 	b = copy.deepcopy(a)
 	b.append(1)
+	print(t*(n-i), end="")
 	for p in range(0,i):
 		b[p+1] = a[p+1] + a[p]
+		print(b[p],end = " ")
+	print(b[i],end = " ")
+	print(b[i+1])
 	a = b
-	print(a)
---
+++ Pack_3/3_2.py
@@ -0,0 +1,29 @@
+class int_rim(int):
+    def rToI(self,c):
+        R={'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        c=0
+        for i in range(len(s)):
+            if i>0 and R[s[i]]>R[s[i-1]]:
+                c+=R[s[i]]-2*R[s[i-1]]
+            else:
+                c+=R[s[i]]
+        print (c)
+        pass
+    def iToR(self,s):
+        I=[[1000,'M'], [900,'CM'],[500,'D'], [400,'CD'], [100,'C'],[90,'XC'], [50,'L'], [40,'XL'],[10,'X'],[9,'IX'],[5,'V'],[4,'IV'],[1,'I']]
+        c=''
+        while s:
+        	for r in range(0, 13):
+        		if s - I[r][0] >= 0:
+        			s-= I[r][0]
+        			c += I[r][1]
+        			break
+        print(c)
+        pass
+
+a=int_rim()
+s=input('Введите число ')
+if s.isnumeric() and int(s) < 4000:
+    a.iToR(int(s))
+else:
+	a.rToI(s)
